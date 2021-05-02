e165bc0af19b28d7f8e129f1250d8fbf6be9b136
Lucas <lzalaquett@uc.cl>
2021-05-01 20:44:36 -0400

    asdasdas

---
+++ config/config.js
@@ -0,0 +1,20 @@
+require('dotenv/config');
+
+module.exports = {
+    development: {
+        database: process.env.DB_DATABASE,
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASWORD,
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        dialect: "postgres"
+    },
+    production: {
+        database: process.env.DB_DATABASE,
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASWORD,
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        dialect: "postgres"
+    }
+}
